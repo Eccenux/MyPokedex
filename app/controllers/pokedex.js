ae013cd3b1552d9daa49f19bfa7f9ff40d4ce8bc
Eccenux <eccenux+git@gmail.com>
2016-08-12 17:56:33 +0200

    Pokedex list basic view: Name, types.

---
+++ app/controllers/pokedex.js
@@ -0,0 +1,41 @@
+/**
+	@file controllers - pokedex
+
+	Copyright:  ©2016 Maciej "Nux" Jaros
+	  License:  CC-BY or MIT
+	            CC-BY: http://creativecommons.org/licenses/by/3.0/
+	            MIT: http://www.opensource.org/licenses/mit-license 
+*/
+
+/**
+ * @param {jQuery} $ jQuery object
+ * @param {mJappisApplication} $mJ Main object of this application
+ */
+(function($, $mJ)
+{
+	//var LOG = new Logger('controller.settings');
+	
+	/**
+	 * Pokedex page.
+	 */
+	$mJ.controller.pokedex = function()
+	{
+		// list
+		$mJ.makeList ('#page-pokedex', '#pokedex-list-container', pgoData.pokedex, {
+			'listTextFunction' : listTextFunction,
+			addActionButton : true
+		});
+	};
+
+	function listTextFunction(item)
+	{
+		return "\
+			<h2>#" + item.number + " " + item.name +"</h2>\
+			<p>\
+				<span class='type-"+item.type1+"'>"+item.type1+"</span>\
+				<span class='type-"+item.type2+"'>"+item.type2+"</span>\
+			</p>\
+		";
+	};
+	
+})(jQuery, window.mJappisApplication);
