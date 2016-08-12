aa166ebee9617287cc282cacf16c41f6706cb50f
Eccenux <eccenux+git@gmail.com>
2016-08-13 01:41:54 +0200

    large icons background-position fix

--- less/pokemons.less.php
+++ less/pokemons.less.php
@@ -1,18 +1,11 @@
 <?php
 	/**
-		A simple CSS/LESS generator.
+		A simple LESS generator.
 	*/
 
 	//
 	$spritesCount = 151;
 	$spritesPerRow = 10;
-	/*
-	$spritesWidth = 128;
-	$spritesHeight = 128;
-	*/
-	// down-sized
-	$spritesWidth = 64;
-	$spritesHeight = 64;
 	
 	//
 	$rows = ceil($spritesCount / $spritesPerRow);
@@ -24,9 +17,9 @@
 	{
 		for($ci = 0; $ci < $cols; $ci++)
 		{
-			$positionX = - $ci * $spritesWidth;
-			$positionY = - $ri * $spritesHeight;
-			echo "\n.pokemon-num$dex {background-position: ${positionX}px ${positionY}px}";
+			$positionX = - $ci;
+			$positionY = - $ri;
+			echo "\n.pokemon-num$dex {background-position: (${positionX} * @size) (${positionY} * @size)}";
 			// stop
 			$dex++;
 			if ($dex > $spritesCount) {
