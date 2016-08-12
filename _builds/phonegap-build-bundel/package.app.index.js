e4588ee6aaf76f5b34999ad446468f3f78cbfce8
Eccenux <eccenux+git@gmail.com>
2016-08-13 00:04:16 +0200

    build

--- _builds/phonegap-build-bundel/package.app.index.js
+++ _builds/phonegap-build-bundel/package.app.index.js
@@ -12,23 +12,81 @@
 
 		$mJ.makeList ('#page-pokedex', '#pokedex-list-container', pgoData.pokedex, {
 			'listTextFunction' : listTextFunction,
-			addActionButton : true
+			'showItemUrlFunction' : function (item) {
+				return '#page-pokemonCard?id='+ item.number;
+			},
+			addActionButton : false
 		});
 	};
 
+	function formatDexNumber(num)
+	{
+		var str = "" + num;
+		var pad = "000";
+		return pad.substring(0, pad.length - str.length) + str;
+	}
+
 	function listTextFunction(item)
 	{
-		return "\
-			<h2>#" + item.number + " " + item.name +"</h2>\
-			<p>\
-				<span class='type-"+item.type1+"'>"+item.type1+"</span>\
-				<span class='type-"+item.type2+"'>"+item.type2+"</span>\
-			</p>\
-		";
+		return ""
+			+"<div class='pokemon-icon pokemon-num"+item.number+"'></div>"
+			+"<h2>#" + formatDexNumber(item.number) + " " + item.name +"</h2>"
+			+"<p>"
+				+"<span class='type-"+item.type1+"'>"+item.type1+"</span>"
+				+(item.type2.length<1 ? "": "<span class='type-"+item.type2+"'>"+item.type2+"</span>")
+			+"</p>"
+		;
 	};
 
 })(jQuery, window.mJappisApplication);
 // pokedex.js, EOF
+// pokemonCard.js, line#0
+
+// EOC
+// EOC
+(function($, $mJ)
+{
+	//var LOG = new Logger('controller.settings');
+// EOC
+	$mJ.controller.pokemonCard = function(parameters)
+	{
+		var item = getItem(parameters);
+		var page = document.getElementById('page-pokemonCard');
+
+
+		if (item == null) {
+			$('.error-message', page).html($mJ.i18n.get('unexpected error'));
+			$('.error-message', page).show();
+			$('.post-validation-content', page).hide();
+			return;
+		} else {
+			$('.error-message', page).hide();
+			$('.post-validation-content', page).show();
+		}
+
+
+		item.type1Class = "type-"+item.type1;
+		item.type2Class = "type-"+item.type2;
+		item.imageClass = "pokemon-icon pokemon-num"+item.number;
+
+
+		var dataBinding = new DataBindings(page);
+		dataBinding.bind(item);
+	};
+// EOC
+	function getItem(parameters)
+	{
+		var item = null;
+		try {
+			var id = parseInt(parameters.id, 10);
+			var item = pgoData.pokedex[id];
+		} catch (e) {
+			item = null;
+		}
+		return item;
+	}
+})(jQuery, window.mJappisApplication);
+// pokemonCard.js, EOF
 // settings.js, line#0
 
 // EOC
