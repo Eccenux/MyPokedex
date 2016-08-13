e10d73fbd70643bf03719b00b41c2c6ee9c052ba
Eccenux <eccenux+git@gmail.com>
2016-08-13 03:37:37 +0200

    multipliers configuration

---
+++ app/data/config.js
@@ -0,0 +1,14 @@
+pgoData.config = {
+	stabMultiplier : 1.25,
+	effectivenesMultipliers : {
+		super : 1.25,
+		neutral : 1,
+		notVery : 0.75,
+		// combinations
+		superNeutral   : 1.25,
+		superSuper     : 1.25,
+		superNotVery   : 1,
+		notVeryNotVery : 0.75,
+		notVeryNeutral : 0.75,
+	}
+};
